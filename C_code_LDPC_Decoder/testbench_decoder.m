f5833b60a0284f11dcdf115026ab53c238cc79f9
KienLeTrung <32816313+KienLeTrung@users.noreply.github.com>
2017-10-25 10:27:23 +0200

    Fix bug in ML

--- C_code_LDPC_Decoder/testbench_decoder.m
+++ C_code_LDPC_Decoder/testbench_decoder.m
@@ -4,7 +4,7 @@ mex ML.c;
 %mex test_generate_H_2.c;
 no_iteration=int16(25);
 %SNR_dB=-2.6:0.2:-2;
-SNR_dB=30;
+SNR_dB=-1.2;
 SNR=10.^(SNR_dB./10);
 E=1;    %average energy of transmitted signal
 muy=0;  %mean of noise
@@ -19,9 +19,9 @@ load('Gen_BG2_Z128_Kb10.mat')
 
 Z=int16(128);
 BG=int16(2);
-rate=0.2;
+rate=0.25;
 Kb=int32(10);
-no_punctured_columns=0;
+no_punctured_columns=10;
 rows=size(no_one_element,2)*Z;
 cols=52*Z;
 
@@ -41,11 +41,12 @@ for m=1:10000
     r=v_transmitted((2*Z+1):(size(v,2)-no_punctured_columns*Z))+w1;                  %received codeword 
     
 %LLR of the channel
-%        p_channel((2*Z+1):(size(v,2)-no_punctured_columns*Z))=(sqrt(2)/std^2).*r;             
-%        p_channel_fixed=int16(round(p_channel*2^7));     %fixed point 9_7
-    
-   [p_channel]=ML(r,std,size(v,2),no_punctured_columns,Z);
-
+       %p_channel((2*Z+1):(size(v,2)-no_punctured_columns*Z))=(sqrt(2)/std^2).*r; 
+%        p_channel((2*Z+1):(size(v,2)-no_punctured_columns*Z))=r;
+%        p_channel_fixed=int16(round(p_channel*2^7));     %fixed point 9_7    
+  % [p_channel]=ML(r,size(v,2),no_punctured_columns,Z);
+  
+[p_channel]=ML(r,no_punctured_columns,Z);
    [v_estimate]=LDPCDecoder(no_iteration,shift_value,Col_position,no_one_element,Z,BG,Kb,rate,p_channel);
 
     if isequal(v,v_estimate)==0
